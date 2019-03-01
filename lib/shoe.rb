a081ea26e8020686fc7116b4919702a7a6c0487f
roy renno <royrenno@yahoo.com>
2019-03-01 02:07:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,15 @@
 # Make your shoe class here!
 class shoe
-  attr_accessor :brand, :color, :size, :material, :condition
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
   def initialize(brand)
     @brand = brand
   end
 
-  def cobble
-    puts "Your shoe has been repaired!"
+  def cobble 
     self.condition = "new"
+    puts "Your shoe has been repaired!"
+    
 end
 end
